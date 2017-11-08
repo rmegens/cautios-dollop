d2354791347aa5f92411e2fa6a0cf1d18a392b49
Rik Megens <rikmegens@work-smarter.nl>
2017-11-08 10:53:03 +0100

    Added tests, georganized files

--- test_winrm.py
+++ test_winrm.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python2.7
 # test_winrm.py
 # version 1.0, d.d. 5 nov 2017 by RME
 # simple python(2.7) script to test winrm connectivity
@@ -6,9 +7,9 @@
 import winrm
 
 # declare variables
-uname=[your-ansible-username-here]
-pword=[your-ansible-password-here]
-managednode=[ip-of-windows-2012r2-managed-node-here]
+uname=Administrator
+pword='-LSKJ?rrLh8'
+managednode=ec2-54-171-94-237.eu-west-1.compute.amazonaws.com
 remotecmd='hostname'
 
 session = winrm.Session(managednode, auth=(uname,pword))
